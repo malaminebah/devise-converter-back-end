a0cf15c0b825bdc16934d5a642015f55a9d667e1
malaprince11 <60395456+malaprince11@users.noreply.github.com>
2024-09-17 15:15:28 +0200

    remove comments

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // Remplacez par l'URL de votre frontend en production
+    origin: 'http://localhost:3001',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
